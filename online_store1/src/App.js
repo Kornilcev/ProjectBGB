deb6a5a44347c3438bed99f8d4c86cbb1c7291c2
Kornilcev <kornilcevkirill@gmail.com>
2024-10-09 22:32:48 +0200

    1

--- online_store1/src/App.js
+++ online_store1/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/header';
+import MainPage from './pages/MainPage/MainPage';
+import CategoriesPage from './pages/CategoriesPage/CategoriesPage';
+import AllProductsPage from './pages/AllProductsPage/AllProductsPage';
+import AllSalesPage from './pages/AllSalesPage/AllSalesPage';
+import CartPage from './pages/CartPage/CartPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div >
-    
+      <Header/>
+      <Routes>
+        <Route path='/' element={<MainPage/>}/>
+        <Route path='/categories' element={<CategoriesPage/>}/>
+        <Route path='/all_products' element={<AllProductsPage/>}/>
+        <Route path='/sales' element={<AllSalesPage/>}/>
+        <Route path='/cart' element={<CartPage/>}/>
+        <Route path='*' element={<NotFoundPage/>}/>
+      </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ online_store1/src/components/Header/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './header.module.css'
+import NavMenu from '../../pages/NavMenu/NavMenu'
+
+export default function Header() {
+  return (
+    <div>
+
+        <NavMenu/>
+    </div>
+  )
+}
